d20fc9a78d3d498429a839e8a46199b746246c81
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-11-11 11:39:13 +1100

    home page styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#home'
+
+  get 'pages/home'
+  get 'pages/about', as: :about
+  get 'pages/hardelements', as: :hardelements
+
+  get 'project/index'
+
+  get 'information/index'
+
+  get 'hardelements', to: 'hardelements#index'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
