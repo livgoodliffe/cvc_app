5b3dde6a491c6abdbba29f89c7e3eab4461da225
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-11-11 11:42:53 +1100

    attempt 2 add address to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_004006) do
+ActiveRecord::Schema.define(version: 2019_11_11_004219) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.integer "score"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
 end
