c08ff1a325f75417c3576c50f99c7fc8cc9808a8
Olivia Goodliffe <ocgoodliffe@gmail.com>
2019-11-11 11:40:39 +1100

    added address to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_062835) do
+ActiveRecord::Schema.define(version: 2019_11_11_004006) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
